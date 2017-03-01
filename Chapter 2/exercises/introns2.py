54b0b4acff42fc135159ed6cd845c23c1b18a554
LAURA <lauratrejo44@gmail.com>
2017-02-28 18:37:41 -0800

    adding files

---
+++ Chapter 2/exercises/introns2.py
@@ -0,0 +1,7 @@
+from __future__ import division
+my_dna = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+exon1 = my_dna[0:63]
+exon2 = my_dna[90:]
+coding_length = len(exon1 + exon2)
+total_length = len(my_dna)
+print(100 * coding_length / total_length)
